7afa232b71b45e245590ed3633ed221b93f5bb36
faizan <faizan-666@outlook.com>
2021-08-30 21:15:12 +0500

    Created a Basic Fiber Web Server

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/andybalholm/brotli v1.0.3 // indirect
 	github.com/gofiber/fiber/v2 v2.18.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/klauspost/compress v1.13.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.29.0 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Could not load .env file")
+	}
+
 	app := fiber.New(fiber.Config{
 		Prefork: true,
 	})
@@ -16,4 +28,8 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World")
 	})
+
+	PORT := fmt.Sprintf(":%s", os.Getenv("PORT"))
+
+	app.Listen(PORT)
 }
